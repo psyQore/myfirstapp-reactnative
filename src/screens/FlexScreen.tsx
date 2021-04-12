7dbb572bcc5d8a10bc5951c05c090c385275a8ea
Juan Galarce <j6alarce@gmail.com>
2021-04-12 15:17:32 -0400

    Ejemplo de justify content

--- src/screens/FlexScreen.tsx
+++ src/screens/FlexScreen.tsx
@@ -17,22 +17,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'purple',
-    flexDirection: 'column',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
   },
   box1: {
-    flex: 1,
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 30,
   },
   box2: {
-    flex: 2,
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 30,
   },
   box3: {
-    flex: 6,
     borderWidth: 2,
     borderColor: 'white',
     fontSize: 30,
